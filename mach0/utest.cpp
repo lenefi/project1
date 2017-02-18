3680e2d1654528e14fb23bee9c8e72fa422a320a
Lars Snekkerhaugen <larssnek@banach03.math.ntnu.no>
2017-02-18 16:36:29 +0100

    vtest mach0 updated

--- mach0/utest.cpp
+++ mach0/utest.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cmath>
-#include "mach0"
+#include "mach0.h"
+#include "utest.h"
+
+using namespace std;
 
 void utest(){
-	expected_value=3.11;
-	double pi_approx=mach0(int n=3);
+	double expected_value=3.11;
+	int n=3;
+	double pi_approx=mach0(n);
 	cout << "expected value Machin formula using calculator: " << expected_value << endl << "vs computed value: " << pi_approx << endl;
-
-
 }
---
+++ mach0/vtest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include "vtest.h"
+#include "mach0.h"
+using namespace std;
+
+void vtest(){
+	ofstream myfile;
+	myfile.open("vtest.txt");
+	myfile << "Verification test: \n";
+	double error, pi_approx;
+	int n;
+	for(int i = 1; i <25; i++){
+		n=pow(2,i);
+		pi_approx=mach0(n);
+		error=abs(M_PI-pi_approx);
+		myfile << "Error for n=" << n << ": " << error << endl;
+	}
+	myfile.close();
+}
