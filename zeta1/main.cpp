da2c7dcfdcc5db9394532f3a9729b6442ece6f6e
Lene Finsveen <lenefi@null14.math.ntnu.no>
2017-02-26 16:19:56 +0100

    distribution

--- zeta1/main.cpp
+++ zeta1/main.cpp
@@ -7,9 +7,10 @@ int main (int argc, char **argv){
 MPI_Status status;
 
 MPI_Init(&argc,&argv);
+int s=2;
+int n=4;
 
-
-test();
+zeta1(s,n);
 
 MPI_Finalize();
 return 0;}
--- zeta1/zeta1.cpp
+++ zeta1/zeta1.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
+#include <cmath>
 #include "mpi.h"
 
-void zeta1(int s, int n)
+void zeta1(int s, int n){
+
 
 int size, rank,i;
 int vec[n];
 
+MPI_Status status;
 MPI_Comm_size(MPI_COMM_WORLD, &size);
 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-tag = 100;
+int tag = 100;
 int r= n%size;
+int m = (n-r)/size;
 
 if (rank ==0) {
 
 	for (i=1; i<=n; i++){
-	double start = 1.0/pow(i,s);
+	double start = 1.0/std::pow(i,s);
 	vec[i-1]= start;
 	}
+
 	for (i=1; i<=size; i++){
-	MPI_Send(start,1,MPI_DOUBLE,i,tag,MPI_COMM_WORLD);}}
+	int vec_temp[m];
+	vec_temp = vec[(i-1*m):i*m-1];
+	MPI_Send(vec_temp,n,MPI_DOUBLE,i,tag,MPI_COMM_WORLD);}}
 		
 else{
-	MPI_Recv(start,1, MPI_DOUBLE,0,tag,MPI_COMM_WORLD, &status);
-}
-
-std::cout << "node: "<< rank  <<  message << "\n";
-
+	MPI_Recv(vec_temp,n, MPI_DOUBLE,0,tag,MPI_COMM_WORLD, &status);}
 
 }
