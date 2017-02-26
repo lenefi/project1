800a62055690cfd8473de48176796f8a3721e2c1
Lene Finsveen <lenefi@null14.math.ntnu.no>
2017-02-26 18:12:37 +0100

    ferdig søndag

--- zeta1/zeta1.cpp
+++ zeta1/zeta1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cmath>
 #include "mpi.h"
+#include <ctime>
+
+clock_t start = clock();
+
 
 void zeta1(int s, int n){
 
@@ -18,17 +22,34 @@ int m = (n-r)/size;
 
 if (rank ==0) {
 
-	for (i=1; i<=n; i++){
+	for (i=1; i<n; i++){
 	double start = 1.0/std::pow(i,s);
 	vec[i-1]= start;
 	}
 
-	for (i=1; i<=size; i++){
-	int vec_temp[m];
-	vec_temp = vec[(i-1*m):i*m-1];
-	MPI_Send(vec_temp,n,MPI_DOUBLE,i,tag,MPI_COMM_WORLD);}}
+	for (i=1; i<size; i++){
+
+	MPI_Send(&vec[(i-1)*m],m,MPI_DOUBLE,i,tag,MPI_COMM_WORLD);}}
 		
 else{
-	MPI_Recv(vec_temp,n, MPI_DOUBLE,0,tag,MPI_COMM_WORLD, &status);}
+	MPI_Recv(&vec,m, MPI_DOUBLE,0,tag,MPI_COMM_WORLD, &status);
+	double pi[size];
+	for (i=1; i<n; i++){
+	pi[i]=std::sum(vec);
+
+	MPI_Send(pi, size, MPI_DOUBLE, 0,tag, MPI_COMM_WORLD);
+	MPI_Recv(pi, size, MPI_DOUBLE, i,tag, MPI_COMM_WORLD, &status);
+	}
+
+
+ }
+
+	
 
 }
+
+}
+
+clock_t end = clock();
+float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+
