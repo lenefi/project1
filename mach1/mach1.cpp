86a9e2e3070c3faf80f0034df3e77876c802812e
Lars Snekkerhaugen <larssnek@banach14.math.ntnu.no>
2017-03-01 17:24:27 +0100

    Updates

--- mach1/mach1.cpp
+++ mach1/mach1.cpp
@@ -3,12 +3,13 @@
 #include <fstream>
 #include <cmath>
 #include "mpi.h"
-
+#include <ctime>
 using namespace std;
 
 void mach1(int n, double* vec)
 {
-	
+	clock_t start;
+	double const corr_value =M_PI/4.;
 	double *vec1=vec;
 	MPI_Status status;
 	int rank, size, tag;
@@ -17,16 +18,22 @@ void mach1(int n, double* vec)
 	tag=100;
 	int r=(2*n)%size; //rest to balance partitioning of array between processes
 	int m=(2*n-r)/size; // least number of elements to each process
-	int ind=0; //indicates how many elements already sent to processes	
 	double* recvec=new double[m+1]; //Array used to receive
-	int p;
-	int s;
+	int p = m + (rank < r ? 1 : 0);
+	double s;
+	int ind=p; //indicates how many elements already sent to processes	
+
+	double sumtest;
+	
 	if(rank==0){
 		double pi=0., a=1./5., b=1./239.;
 		for (int i=1; i<=n; ++i) {
 			vec[2*(i-1)]=4.0*pow(-1.0,i-1)/(2.0*i-1)*pow(a,2*i-1);
 			vec[2*(i-1)+1]=pow(-1.0,i)/(2.0*i-1)*pow(b,2*i-1); //multiply by -1 so that we can just sum up for each process
 		}
+                // set the size for root
+                int const p0 = p;
+                // update offset for rank 1
 		for(int i=1;i< size;i++){
 			if(r>0){
 				r-=1;
@@ -37,18 +44,32 @@ void mach1(int n, double* vec)
 			}
 
 			vec1=vec+ind;
-			MPI_Send(vec1,p,MPI_FLOAT,i,tag,MPI_COMM_WORLD);
+			MPI_Send(vec1,p,MPI_DOUBLE,i,tag,MPI_COMM_WORLD);
 			ind+=p;
 		}
+                // sum for root
+		p=p0;
+		recvec=vec;
 	}
 	else{
-		MPI_Recv(recvec, p, MPI_FLOAT, 0, tag, MPI_COMM_WORLD, &status);
-		for(int i=0; i<p;i++){
-			s+=recvec[i];
-		}
-		cout << s << endl;	
-	}	
+		MPI_Recv(recvec, p, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+	}
+	s=0.0;
+	for(int i=0; i<p;i++){
+		s+=recvec[i];
+	}
+	double Sn;
+	MPI_Reduce(&s, &Sn,1,MPI_DOUBLE, MPI_SUM,0,MPI_COMM_WORLD);
 	
+	if(rank==0){
+		
+		cout<< scientific << "The global sum: " <<4.0*Sn << endl;
+		cout <<scientific  << "Error for n=" << n<<" and the number of processes=" <<size << ": " << abs(corr_value-Sn) << endl;
+		
+		double duration = (clock()-start)/(double)CLOCKS_PER_SEC;	
+		cout << "Runtime: " << duration << endl;
+	}
+
 /*
 // This will check if vec1 and vec2 is made correctly
 	ofstream myfile;
@@ -78,5 +99,4 @@ void mach1(int n, double* vec)
 	printf("node %d: %13s\n", rank, message);
 */
 	MPI_Finalize();
-
 }
--- mach1/main.cpp
+++ mach1/main.cpp
@@ -1,9 +1,9 @@
 #include "mach1.h"
 #include "mpi.h"
-
+#include <cmath>
 int main(int argc,char **argv)
 {
-	int n =4;
+	int n =pow(2, 15);
 	double *vec=new double[2*n];
 	MPI_Init(&argc, &argv);
 	mach1(n,vec);
