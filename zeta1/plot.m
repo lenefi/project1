c0dd1962f2424c38ac6ee5c00c7fe2c069bef268
Lene Finsveen <lenefi@ntnu.no>
2017-03-03 13:40:23 +0100

    matlab file

---
+++ zeta1/plot.m
@@ -0,0 +1,21 @@
+clear all;
+close all;
+clc;
+
+n = fopen("data.txt");
+n =n (2:length(n));
+
+data = fopen("data.txt");
+err = data(:,1);
+tid = data(:,2);
+
+figure
+plot(n, err);
+xlabel('n iterations');
+ylabel('error of method');
+
+
+figure
+plot(n, tid);
+xlabel('n iterations');
+ylabel('time required');
