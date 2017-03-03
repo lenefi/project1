ce1fcd6abaa75bcfc68cb10ca4de8bfcf978b111
Lene Finsveen <Lene Finsveen>
2017-03-03 13:57:14 +0100

    'matlab'

--- zeta1/plot.m
+++ zeta1/plot.m
@@ -2,10 +2,10 @@ clear all;
 close all;
 clc;
 
-n = fopen("data.txt");
+n = fopen('data.txt');
 n =n (2:length(n));
 
-data = fopen("data.txt");
+data = fopen('data.txt');
 err = data(:,1);
 tid = data(:,2);
 
